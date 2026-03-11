5af25c2275681d3af317ab580ec9d0f88c50d285
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2026-03-11 13:21:40 -0600

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Outfit } from 'next/font/google';
+import { HeroProvider } from '@/providers/HeroProvider';
+
+import '../styles/globals.css';
+
+const outfitSans = Outfit({
+  variable: '--font-outfit-sans',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Sistema de Roles | Iglesia Cristiana Evangélica Príncipe de Paz',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='es-MX'>
+      <body className={`${outfitSans.variable} antialiased`}>
+        <HeroProvider>{children}</HeroProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from 'next/navigation';
+import { getSession } from '@/modules/auth/infrastructure/auth.repository';
+
+export default async function Page() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect('/login');
+  }
+
+  redirect('/dashboard');
+}
