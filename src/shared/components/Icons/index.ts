6c60116811f996a7be2d197454f5f4d1edc6e8b5
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2026-03-13 11:51:16 -0600

    feat: add icons

--- src/shared/components/Icons/index.ts
+++ src/shared/components/Icons/index.ts
@@ -1,2 +1,6 @@
+export * from './DocumentDownloadOutlined';
+export * from './EditOutlined';
 export * from './EyeOutlined';
 export * from './EyeSlashOutlined';
+export * from './TrashOutlined';
+export * from './WhatsappOutlined';
