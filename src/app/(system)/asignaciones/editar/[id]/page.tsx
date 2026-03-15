941df8d8b63033d27bfdd758849335526b5f0a98
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2026-03-15 17:06:21 -0600

    feat: add new and edit page

---
+++ src/app/(system)/asignaciones/editar/[id]/page.tsx
@@ -0,0 +1,15 @@
+import { AssignmentForm } from '@/modules/assignments/presentation/AssignmentForm';
+import type { PageProps } from '@/shared/types';
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+
+  return (
+    <div className='flex flex-col gap-10'>
+      <AssignmentForm
+        action='update'
+        assignmentId={id}
+      />
+    </div>
+  );
+}
---
+++ src/app/(system)/asignaciones/nuevo/page.tsx
@@ -0,0 +1,9 @@
+import { AssignmentForm } from '@/modules/assignments/presentation/AssignmentForm';
+
+export default function Page() {
+  return (
+    <div className='flex flex-col gap-10'>
+      <AssignmentForm />
+    </div>
+  );
+}
